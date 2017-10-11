b54d69f986efc62e3842d0af7b8712fd4661fcd1
siddarthgautham <31670386+siddarthgautham@users.noreply.github.com>
2017-10-11 08:13:41 -0700

    Add files via upload

---
+++ if.py
@@ -0,0 +1,14 @@
+var1 = 0
+print("lowerlimit")
+i = int(input()) 
+print("upperlimit")
+j = int(input())
+          
+for var1 in range(i,j):
+     print("val of Var" , var1)
+     var1 = var1+1
+
+print("EOL")
+
+
+        
