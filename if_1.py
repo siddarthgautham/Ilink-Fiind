dce7199cf6b282cadc263917956d4a3c0afb8eb3
siddarthgautham <31670386+siddarthgautham@users.noreply.github.com>
2017-10-11 15:10:38 -0700

    Add files via upload

---
+++ if_1.py
@@ -0,0 +1,14 @@
+var1 = 0
+print("lowerlimit")
+i = 5
+print("upperlimit")
+j = 10
+          
+for var1 in range(i,j):
+     print("val of Var" , var1)
+     var1 = var1+1
+
+print("EOL")
+
+
+        
---
+++ if_2.py
@@ -0,0 +1,14 @@
+var1 = 0
+print("lowerlimit")
+i = 10
+print("upperlimit")
+j = 15
+          
+for var1 in range(i,j):
+     print("val of Var" , var1)
+     var1 = var1+1
+
+print("EOL")
+
+
+        
